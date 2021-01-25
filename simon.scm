8e0299eaf72c7c78b7aeb4d493a374512cd18271
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2021-01-25 08:35:13 +0200

    Create simon.scm

---
+++ simon.scm
@@ -0,0 +1,5 @@
+(string=? "I am alone" "I am alone")      ;; => #t
+(string=? "I feel young" "I feel young")  ;; => #t
+(string=? "I play a game" "I play a game");; => #t
+(string=? "abc" "abc")                    ;; => #t
+(string=? "do androids dream of the electric sheep?"  "do androids dream of the electric sheep?")
